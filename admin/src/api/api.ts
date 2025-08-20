7e0ca97d4d08f0b849330203ec8d338ef27dc2ad
Mohamed-bek <mohamedbekkouche0302@gmail.com>
2025-08-20 01:35:47 +0100

    fix vercel config

--- admin/src/api/api.ts
+++ admin/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken, setAccessToken, logout } from "../hooks/useAuth";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api" || "http://localhost:8000/api",
   withCredentials: true,
 });
 
